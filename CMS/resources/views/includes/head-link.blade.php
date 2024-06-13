0e794a9bfe2d9dfed147aadd029f5d0e314deb4c
Taha Tehseen <mtahamtehseen@gmail.com>
2024-06-13 10:44:10 +0500

    6-12-24

---
+++ CMS/resources/views/includes/head-link.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="https://unpkg.com/@shopify/polaris@13.3.0/build/esm/styles.css">
+<link rel="stylesheet" href="css/style.css">
+<link href="https://cdn.jsdelivr.net/npm/material-icons@1.13.12/css/material-icons.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
---
+++ CMS/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>App Instruction</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include("includes.head-link")
+
+</head>
+
+<body>
+    @include("includes.navbar")
